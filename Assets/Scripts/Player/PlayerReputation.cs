ea4c604c68959e3302955d51a6c9a51cb1c72675
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2024-01-26 23:31:51 +0800

    Reputation Script and Noise Shader Graph

--- Assets/Scripts/Player/PlayerReputation.cs
+++ Assets/Scripts/Player/PlayerReputation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reputation : MonoBehaviour
+public class PlayerReputation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
