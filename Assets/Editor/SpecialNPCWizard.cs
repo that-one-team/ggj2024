97b072375a94874b15f94770ae927734859d4a93
zsfer <seiferalbacete@gmail.com>
2024-01-26 23:38:06 +0800

    special npc tools

---
+++ Assets/Editor/SpecialNPCWizard.cs
@@ -0,0 +1,40 @@
+// author: @zsfer
+using UnityEngine;
+using UnityEditor;
+
+public class SpecialNPCWizard : ScriptableWizard
+{
+    public string NPCName;
+    public HumorStats Stats;
+    public ClothingItemData[] Clothes;
+
+    [MenuItem("TOT/Create Special NPC")]
+    private static void MenuEntryCall()
+    {
+        DisplayWizard<SpecialNPCWizard>("Create Special NPC");
+    }
+
+    private void OnWizardCreate()
+    {
+        NPCData data = CreateInstance<NPCData>();
+        data.Stats = Stats;
+        data.Name = NPCName;
+        data.Clothes = Clothes;
+
+        var path = AssetDatabase.GUIDToAssetPath(AssetDatabase.CreateFolder("Assets/Resources/NPCs", NPCName));
+        AssetDatabase.CreateAsset(data, path + $"/{NPCName}Data.asset");
+
+        GameObject go = new(NPCName);
+        var npcBehavior = go.AddComponent<NPCBehavior>();
+        npcBehavior.SpecialCharacterData = data;
+        go.SetActive(false);
+
+        PrefabUtility.SaveAsPrefabAsset(go, path + $"/{NPCName}.prefab");
+
+        AssetDatabase.SaveAssets();
+        DestroyImmediate(go);
+
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = data;
+    }
+}
